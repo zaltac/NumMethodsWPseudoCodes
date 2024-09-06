ca7d50f6adc78007335055aa894d950434edaebb
Zekeriya ALTAÇ <153484393+zaltac@users.noreply.github.com>
2024-09-06 10:50:31 +0300

    Add files via upload

---
+++ Chapter01/Code1-4-Factorial.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+// ==============================================================================
+//  The main program to test FACTORIAL
+// ==============================================================================
+int factorial(int n);   // Declare factorial function
+
+int main() {
+    int n, fact;
+    printf("Enter n\n");
+    scanf("%d", &n);
+    
+    fact = factorial(n);
+    
+    printf("%d! = %d\n", n, fact);
+    return 0;
+}
+
+int factorial(int n) {
+// ==================================================================================
+// CODE1.4-Quadratic_Eq.c. A C function module for implementing Pseudocode 1.4.
+//
+// NUMERICAL METHODS FOR SCIENTISTS AND ENGINEERS With Pseudocodes.
+// First Edition. (c) By Zekeriya ALTAÇ (2024).
+// ISBN: 978-1-032-75474-1 (hbk)
+// ISBN: 978-1-032-75642-4 (pbk)
+// ISBN: 978-1-003-47494-4 (ebk)
+// 
+// DOI : 10.1201/9781003474944
+// C&H/CRC PRESS, Boca Raton & London.
+// 
+// This free software is complimented by the author to accompany the textbook.
+// E-mail: altacz@gmail.com.
+
+//                                                                                   
+// DESCRIPTION: A recursive function for computing n!                                
+//                                                                                   
+// INPUT ARGUMENT                                                                    
+//   n    :: Integer (n>=0)                                                          
+//                                                                                   
+// ON EXIT                                                                           
+//  Result:: n!                                                                      
+//                                                                                   
+// REVISION DATE :: 03/21/2024                                                       
+//                                                                                   
+// ==================================================================================  
+    if (n < 0) {
+        printf("Error, Illegal Input\n");
+        printf("Argument should be n>=0, you entered %d\n", n);
+        return -1;
+    } else {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
+
---
+++ Chapter01/Code1-4-Factorial.f95
@@ -0,0 +1,55 @@
+PROGRAM TestFactorial
+! ==============================================================================
+!  The main program to test RECURSIVE FUNCTION FACTORIAL
+! ==============================================================================
+INTEGER :: n, factorial
+
+WRITE(*,"(A7)") "Enter n" 
+READ(*,*) n
+
+WRITE(*,*) n,"! = ",FACTORIAL(n)
+
+END PROGRAM
+
+INTEGER RECURSIVE FUNCTION FACTORIAL(n) RESULT(fact)
+!  ==================================================================================
+!  CODE1.4-Quadratic_Eq.f95. A fortran module for implementing Pseudocode 1.4.          
+! 
+!  NUMERICAL METHODS FOR SCIENTISTS AND ENGINEERS With Pseudocodes.
+!  First Edition. (c) By Zekeriya ALTAÇ (2024).
+!  ISBN: 978-1-032-75474-1 (hbk)
+!  ISBN: 978-1-032-75642-4 (pbk)
+!  ISBN: 978-1-003-47494-4 (ebk)
+!  
+!  DOI : 10.1201/9781003474944
+!  C&H/CRC PRESS, Boca Raton & London.
+!  
+!  This free software is complimented by the author to accompany the textbook.
+!  E-mail: altacz@gmail.com.
+
+!                                                                                    
+!  DESCRIPTION: A recursive function for computing n!                                
+!                                                                                    
+!  INPUT ARGUMENT                                                                    
+!    n    :: Integer (n>=0)                                                          
+!                                                                                    
+!  ON EXIT                                                                           
+!   Result:: n!                                                                      
+!                                                                                    
+!  REVISION DATE :: 03/21/2024                                                       
+!                                                                                    
+!  ==================================================================================
+INTEGER, INTENT(IN) :: n    ! Input Variable
+! 
+IF(n<0) THEN
+  write(*,*) "Error, Illegal Input"
+  write(*,*) "Argument should be n>=0, you entered ",n
+  stop
+ELSE
+   IF(n<=1) THEN
+       fact = 1
+   ELSE
+       fact = n*FACTORIAL(n-1)
+   ENDIF  
+ENDIF  
+END FUNCTION FACTORIAL 
