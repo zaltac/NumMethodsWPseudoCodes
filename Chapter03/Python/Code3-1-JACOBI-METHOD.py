025f42ea95758b46f7428d6642fc43bac3a448a6
Zekeriya ALTAÇ <153484393+zaltac@users.noreply.github.com>
2024-12-21 08:37:54 +0300

    Update Code3-1-JACOBI-METHOD.py

--- Chapter03/Python/Code3-1-JACOBI-METHOD.py
+++ Chapter03/Python/Code3-1-JACOBI-METHOD.py
@@ -1,6 +1,127 @@
 import numpy as np
 
-# Program to test Jacobi method
+#  ==================================================================================
+#  CODE3.1-JACOBI.PY. A Python module implementing JACOBI_DRV module of Pseudocode 3.1.                                
+# 
+#  NUMERICAL METHODS FOR SCIENTISTS AND ENGINEERS: WITH PSEUDOCODES
+#  First Edition. (c) By Zekeriya ALTAÇ (2024).
+#  ISBN: 978-1-032-75474-1 (hbk)
+#  ISBN: 978-1-032-75642-4 (pbk)
+#  ISBN: 978-1-003-47494-4 (ebk)
+#  
+#  DOI : 10.1201/9781003474944
+#  C&H/CRC PRESS, Boca Raton, FL, USA & London, UK.
+#  
+#  This free software is complimented by the author to accompany the textbook.
+#  E-mail: altacz@gmail.com.
+#  
+#  DESCRIPTION: A subroutine to to perform one step Jacobi iteration and compute               
+#     the Euclidean norm of the displacement vector.                                           
+#                                                                                              
+#  ON ENTRY                                                                                    
+#     n   :: Number of equations (size of A);                                                  
+#     A   :: Input coefficient matrix (nxn);                                                  
+#     b   :: Array of length n containing the right-hand;                                      
+#     x   :: Array of length n containing the estimate at (p+1)'th step;                       
+#     xo  :: Array of length n containing the estimate at p'th step.                           
+#                                                                                              
+#  ON EXIT                                                                                     
+#     x   :: Array of length n containing the estimated solution;                              
+#     del :: Maximum absolute error achieved.                                                  
+#                                                                                              
+#  USES                                                                                        
+#   ENORM:: User-defined function calculating the Euclidean vector (L2 norm) of a vector.      
+#                                                                                              
+#  REVISION DATE :: 11/09/2024                                                                 
+#  ==================================================================================
+def jacobi_drv(n, a, b, xo):  
+    d = np.zeros(n)  
+    x = np.zeros(n)  
+    for i in range(n):
+        sums = 0.0
+        for j in range(n):
+            if(i!=j):
+                sums += a[i, j] * xo[j]
+    
+        x[i] =  (b[i] - sums) / a[i, i]
+        d[i] = x[i] - xo[i]
+    
+    delta = ENorm(d)
+    return  (delta, x) # End of module JACOBI_DRV
+
+#  ==================================================================================
+#  CODE3.1-JACOBI.PY. A Python module implementing Pseudocode 3.1.                                
+# 
+#  NUMERICAL METHODS FOR SCIENTISTS AND ENGINEERS: WITH PSEUDOCODES
+#  First Edition. (c) By Zekeriya ALTAÇ (2024).
+#  ISBN: 978-1-032-75474-1 (hbk)
+#  ISBN: 978-1-032-75642-4 (pbk)
+#  ISBN: 978-1-003-47494-4 (ebk)
+#  
+#  DOI : 10.1201/9781003474944
+#  C&H/CRC PRESS, Boca Raton, FL, USA & London, UK.
+#  
+#  This free software is complimented by the author to accompany the textbook.
+#  E-mail: altacz@gmail.com.
+#  
+#  DESCRIPTION: A module to iteratively solve Ax=b using the Jacobi method.                
+#                                                                                              
+#  ON ENTRY                                                                                    
+#     n   :: Number of equations (size of A);                                                  
+#     A   :: Input coefficient matrix (nxn);                                                  
+#     b   :: Array of length n containing the right-hand;                                      
+#     x   :: Array of length n containing the estimate at (p+1)'th step;                       
+#     xo  :: Array of length n containing the initial guess, or iterates at estimate at p'th st
+#    eps  :: Convergence tolerance;                                                            
+#   maxit :: Maximum permitted number of iterations.                                           
+#                                                                                              
+#  ON EXIT                                                                                     
+#     x   :: Array of length n containing the estimated solution;                              
+#   iter  :: Total number of iterations performed;                                             
+#   error :: L2 norm of the displacement vector.                                               
+#                                                                                              
+#  USES                                                                                        
+#    JACOBI_DRV :: Accompanying module performing one step Jacobi iteration.               
+#                                                                                              
+#  REVISION DATE :: 11/09/2024                                                                 
+#  ==================================================================================
+def jacobi(n, eps, a, b, xo, maxit):
+    del0 = 1.0
+    p = 0
+    x = xo.copy()
+    while (del0>eps and p<maxit):
+        p += 1
+        (del1,x)=jacobi_drv(n, a, b, xo)
+        # printout the ietartion progress 
+        print(f" iter={p:5}  delta = {del1:8.2e}")
+        xo[:] = x
+        del0 = del1
+    
+    if p == maxit:
+        print(f"\nJacobi method failed to converge after {maxit} iterations\nwithin the specified EPS tolerance.")
+    error=del1
+    iter=p
+    return (iter, error, x) # End of module jacobi
+
+
+def ENorm(x):
+    """
+    DESCRIPTION: A funcion module to compute Euclidean (L2) norm of a vector.
+
+    On ENTRY
+        n  :: Length of vector; 
+        x  :: An array (vector)) of length n.
+    """
+    n=len(x)
+    delta = 0.0
+    for i in range(n):
+        delta += x[i] * x[i]
+    
+    return np.sqrt(delta)  # End of E-norm module
+
+# ==============================================================================
+#  The main program to test JACOBI.PY
+# ==============================================================================
 def test_jacobi_method():
     n = 3
     a = np.zeros((n, n))  # Coefficient matrix
@@ -30,92 +151,5 @@ def test_jacobi_method():
     print("\n-----------------------")
     print("Total no of iterations = ",iter,"Maximum Error = ",error)
 
-def jacobi(n, eps, a, b, xo, maxit):
-    """
-    DESCRIPTION: A module to solve Ax=b iteratively using the Jacobi method.
-
-    On ENTRY
-        n   :: Number of equations (size of matrix A); 
-        a   :: Coefficient matrix (n×n);  
-        b   :: Array of lenght n containing the RHS;
-        xo  :: Array of lenght n containing the initial guess;   
-        eps :: Convergence tolerance;
-        maxit:: Maximum number of iterations allowed.
-        
-    On EXIT:
-        x   :: Array of lenght n containing the final estimate for the solution; 
-        iter:: Total number of iterations performed;
-        error:: Euclidean norm of the error.
-    
-    USES
-        jacobi_drv:: Driver module for a sor iteration.
-    """
-    del0 = 1.0
-    p = 0
-    x = xo.copy()
-    while (del0>eps and p<maxit):
-        p += 1
-        (del1,x)=jacobi_drv(n, a, b, xo)
-        # printout the ietartion progress 
-        print(f" iter={p:5}  delta = {del1:8.2e}")
-        xo[:] = x
-        del0 = del1
-    
-    if p == maxit:
-        print(f"\nJacobi method failed to converge after {maxit} iterations\nwithin the specified EPS tolerance.")
-    error=del1
-    iter=p
-    return (iter, error, x) # End of module jacobi
-
-    
-def jacobi_drv(n, a, b, xo):
-    """
-    DESCRIPTION: A module to perform one step jacobi iteration.
-
-    On ENTRY
-        n   :: Number of equations (size of matrix A); 
-        a   :: Coefficient matrix (n×n);  
-        b   :: Array of lenght n containing the RHS;
-        xo  :: Array of lenght n containing the initial guess;   
-        eps :: Convergence tolerance;
-        maxit:: Maximum number of iterations allowed.
-        
-    On EXIT:
-        x   :: Array of lenght n containing the final estimate for the solution; 
-        iter:: Total number of iterations performed;
-        error:: Euclidean norm of the error
-    
-    USES
-        Enorm  :: Euclidean norm of a vector,
-    """    
-    d = np.zeros(n)  
-    x = np.zeros(n)  
-    for i in range(n):
-        sums = 0.0
-        for j in range(n):
-            if(i!=j):
-                sums += a[i, j] * xo[j]
-    
-        x[i] =  (b[i] - sums) / a[i, i]
-        d[i] = x[i] - xo[i]
-    
-    delta = ENorm(d)
-    return  (delta, x) # End of module JACOBI_DRV
-
-def ENorm(x):
-    """
-    DESCRIPTION: A funcion module to compute Euclidean (L2) norm of a vector.
-
-    On ENTRY
-        n  :: Length of vector; 
-        x  :: An array (vector)) of length n.
-
-    On EXIT
-      ENorm:: Euclidean Norm of the vector
-    """
-    n=len(x)
-    delta = 0.0
-    for i in range(n):
-        delta += x[i] * x[i]
-    
-    return np.sqrt(delta)  # End of E-norm module
+if __name__ == "__main__":
+    test_jacobi_method()  
