e6dc9c6d85578cfbae86e1fa6c1c798129f27ce2
Zekeriya ALTAÇ <153484393+zaltac@users.noreply.github.com>
2025-03-05 10:57:30 +0300

    Update Code9-1-Explicit Euler.c

--- Chapter09/C/Code9-1-Explicit Euler.c
+++ Chapter09/C/Code9-1-Explicit Euler.c
@@ -57,7 +57,7 @@ void Explicit_Euler(double h, double x0, double y0, double xlast) {
 //   fabs :: Built-in Intrinsic function returning the absolute value of a real value;
 //   fcn  :: User-defined external function providing y'=f(x,y).                               
 //                                                                                             
-// REVISION DATE :: 03/05/2024                                                                 
+// REVISION DATE :: 03/05/2025                                                                 
 // ==================================================================================
     double x = x0;
     double y = y0;
